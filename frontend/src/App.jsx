bb25f6c7824ab728f7f774de54c8d1d0dfe7e2bd
hrsnk21 <155194869+hrsnk21@users.noreply.github.com>
2024-12-04 20:07:38 +0530

    added maps routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,39 @@ import UserSignup from './pages/UserSignup'
 import Captainlogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import Start from './pages/Start'
-
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
+        <Toaster/>
         <Routes>
            <Route path='/' element={ <Start/>} />
            <Route path='/login' element={<UserLogin />} />
            <Route path='/signup' element={<UserSignup />} />
            <Route path='/captain-login' element={<Captainlogin />} />
            <Route path='/captain-signup' element={<CaptainSignup />} />
+           <Route path='/home'
+              element={
+                     <UserProtectWrapper>
+                          <Home />
+                     </UserProtectWrapper>
+                     } />
+           <Route path='/user/logout'
+              element={<UserProtectWrapper>
+                          <UserLogout />
+                       </UserProtectWrapper>
+                     } />
+           <Route path='/captain-home'
+              element={<CaptainProtectWrapper>
+                          <CaptainHome />
+                       </CaptainProtectWrapper>
+                     } />
+           <Route path='/captain/logout' 
+              element={<CaptainProtectWrapper>
+                         <CaptainLogout />
+                      </CaptainProtectWrapper>
+                    } />
         </Routes>
     </div>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import 'remixicon/fonts/remixicon.css'
+
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <CaptainContext>
+    <UserContext>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </UserContext>
+  </CaptainContext>
 )
