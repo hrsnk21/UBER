b83c70b12c7617bdf43fdcfdb135c12a263c5ff5
hrsnk21 <155194869+hrsnk21@users.noreply.github.com>
2024-12-04 16:08:11 +0530

    completed signup and login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Start from './pages/Start'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <Routes>
+           <Route path='/' element={ <Start/>} />
+           <Route path='/login' element={<UserLogin />} />
+           <Route path='/signup' element={<UserSignup />} />
+           <Route path='/captain-login' element={<Captainlogin />} />
+           <Route path='/captain-signup' element={<CaptainSignup />} />
+        </Routes>
+    </div>
   )
 }
 
